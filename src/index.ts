499a38e8df166487d4d7ff618e8dcdee3a608de0
rwambui <rosemwangi305@gmail.com>
2022-10-30 00:00:24 +0300

    created variable condition checks

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,19 @@
  dotenv.config() //load env variables from local .env
 
 
+ 
+ /**
+ * App Variables
+ * check if node js loaded the env variable port into process.env
+ * If yes, parse its value as a number(1) type (ts)
+ */
 
+// condition pass check
+if(!process.env.PORT){
+  process.exit(1)
+}
+
+// If yes, parse its value as a number(1) type (ts), and initiate an instance of Express app: otherwise exit
+const PORT: number = parseInt(process.env.PORT as string, 10)
+
+const app = express()
