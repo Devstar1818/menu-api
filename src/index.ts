cbb91d7ba5cc61a0f6d2bbb8d35943230fe2addd
rwambui <rosemwangi305@gmail.com>
2022-10-29 23:59:18 +0300

    imported required external modules

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Required External Modules
+ */
+ import express  from 'express';
+ import * as dotenv from 'dotenv';
+ import cors from 'cors';
+ import helmet from 'helmet';
+ 
+
+ dotenv.config() //load env variables from local .env
+
+
+
